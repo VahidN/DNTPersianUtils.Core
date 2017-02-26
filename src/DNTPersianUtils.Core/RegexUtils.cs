c4397cd3b665df13d9e5cc254d96c56d964f53a1
VahidN <vahid_nasiri@yahoo.com>
2017-02-26 13:05:48 +0330

    Added .NET 4.x Support.

--- src/DNTPersianUtils.Core/RegexUtils.cs
+++ src/DNTPersianUtils.Core/RegexUtils.cs
@@ -14,10 +14,18 @@ namespace DNTPersianUtils.Core
         public static readonly TimeSpan MatchTimeout = TimeSpan.FromSeconds(3);
 
         private static readonly Regex _matchAllTags =
-            new Regex(@"<(.|\n)*?>", options: RegexOptions.Compiled | RegexOptions.IgnoreCase, matchTimeout: MatchTimeout);
+            new Regex(@"<(.|\n)*?>", options: RegexOptions.Compiled | RegexOptions.IgnoreCase
+#if !NET40
+                , matchTimeout: MatchTimeout
+#endif
+                );
 
         private static readonly Regex _matchArabicHebrew =
-            new Regex(@"[\u0600-\u06FF,\u0590-\u05FF]", options: RegexOptions.Compiled | RegexOptions.IgnoreCase, matchTimeout: MatchTimeout);
+            new Regex(@"[\u0600-\u06FF,\u0590-\u05FF]", options: RegexOptions.Compiled | RegexOptions.IgnoreCase
+#if !NET40
+                , matchTimeout: MatchTimeout
+#endif
+                );
 
         /// <summary>
         /// آیا عبارت مدنظر حاوی حروف فارسی است؟
