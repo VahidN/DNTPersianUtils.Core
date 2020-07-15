011a536e2b58b141f87a66c51c1d81dbb4681ba9
Vahid Nasiri <vahid_nasiri@yahoo.com>
2020-07-15 08:53:21 +0430

    Add PerformanceTests

---
+++ src/DNTPersianUtils.Core.PerformanceTests/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Environments;
+
+namespace DNTPersianUtils.Core.PerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runBenchmarks();
+        }
+
+        private static void runBenchmarks()
+        {
+            var config = ManualConfig.Create(DefaultConfig.Instance)
+                                .With(BenchmarkDotNet.Analysers.EnvironmentAnalyser.Default)
+                                .With(BenchmarkDotNet.Exporters.MarkdownExporter.GitHub)
+                                .With(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default)
+                                .With(StatisticColumn.Mean)
+                                .With(StatisticColumn.Median)
+                                .With(StatisticColumn.StdDev)
+                                .With(StatisticColumn.OperationsPerSecond)
+                                .With(BaselineRatioColumn.RatioMean)
+                                .With(RankColumn.Arabic)
+                                .With(Job.Default.With(CoreRuntime.Core31)
+                                    .WithIterationCount(300)
+                                    .WithInvocationCount(160)
+                                    .WithIterationTime(TimeInterval.FromSeconds(1000))
+                                    .WithWarmupCount(4)
+                                    .WithLaunchCount(1));
+            BenchmarkRunner.Run<BenchmarkTestsForStringReplace>(config);
+        }
+    }
+}
