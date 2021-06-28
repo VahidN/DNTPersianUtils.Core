fc6205fbac36f40f4cf0f0cc784161fee8a0dfc5
Vahid Nasiri <vahid_nasiri@yahoo.com>
2021-06-28 14:57:11 +0430

    Remove RemoveLastNullRow of CreatePersianMonthCalendar

--- src/DNTPersianUtils.Core/PersianMonthCalendar.cs
+++ src/DNTPersianUtils.Core/PersianMonthCalendar.cs
@@ -50,7 +50,25 @@ namespace DNTPersianUtils.Core
                 k++;
             }
 
+            RemoveLastNullRow(monthCells);
             return monthCells;
+        }
+
+        private static void RemoveLastNullRow(List<PersianDay?> monthCells)
+        {
+            // Calendar rows
+            // 0,  1,  2,  3,  4,  5,  6
+            // 7,  8,  9,  10, 11, 12, 13
+            // 14, 15, 16, 17, 18, 19, 20
+            // 21, 22, 23, 24, 25, 26, 27
+            // 28, 29, 30, 31, 32, 33, 34
+            // 35, 36, 37, 38, 39, 40, 41
+
+            const int beginningOfLastRowIndex = 35;
+            if (monthCells[beginningOfLastRowIndex] is null)
+            {
+                monthCells.RemoveRange(35, 7);
+            }
         }
     }
 }
