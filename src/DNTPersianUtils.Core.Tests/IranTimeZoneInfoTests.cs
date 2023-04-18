48bf79a0df74b77dd9e045b74cd83fbc6cb889b1
VahidN <vahid_nasiri@yahoo.com>
2023-04-18 09:36:10 +0330

    Add IranTimeZoneInfoTests

---
+++ src/DNTPersianUtils.Core.Tests/IranTimeZoneInfoTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DNTPersianUtils.Core.Tests;
+
+[TestClass]
+public class IranTimeZoneInfoTests
+{
+    [TestMethod]
+    public void Test_IranDaylightTime_works()
+    {
+        var irTz = IranTimeZoneInfo.Instance;
+        var isDaylightSavingTime = irTz.IsDaylightSavingTime(new DateTime(1402, 01, 13, new PersianCalendar()));
+        Assert.IsFalse(isDaylightSavingTime);
+    }
+}
